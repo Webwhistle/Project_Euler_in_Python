d2eddd3ee7bee30c5a6820e8451714701432fe57
Webwhistle <57767417+Webwhistle@users.noreply.github.com>
2020-02-21 00:20:09 +0100

    Finished the problem
    Solved with two functions, one that generates palindromes and one that generates three-digit factors

--- euler4.py
+++ euler4.py
@@ -1,10 +1,24 @@
-#Beginning attempt at problem 4
+#Generate all six-digit palindromes
 def palindrome():
-    number = [0,0,0,0,0]
-    for i in range(10):
-        for j in range(5):
-            number[j] = i
-    return number
-    #return([int(i) for i in str(number)])
+    palindromes = []
+    number = [0,0,0,0,0,0]
+    for i in range(9,-1,-1):
+        number[0], number[-1] = i, i
+        for j in range(9,-1,-1):
+            number[1], number[-2] = j, j
+            for k in range(9,-1,-1):
+                number[2], number[-3] = k,k
+                palindromes.append(int(''.join(str(v) for v in number)))
+    palindromes = [ele for ele in palindromes if ele > 99999]
+    return palindromes
 
-print(palindrome())
+#Find all three-digit factors to all six-digit palindromes
+def factors():
+    factors = []
+    palindromes = palindrome()
+    for i in palindromes:
+        for j in range(999,99,-1):
+            if i%j == 0 and i/j < 1000:
+                print([i,j,i/j])
+
+factors()
