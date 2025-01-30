9dc3bf2627ae01a873b40ffe7479275aabe79f77
Webwhistle <57767417+Webwhistle@users.noreply.github.com>
2025-01-30 22:38:52 +0100

    Update euler11.py

--- euler11.py
+++ euler11.py
@@ -7,25 +7,24 @@ __location__ = os.path.realpath(
 with open(os.path.join(__location__, 'euler11.txt')) as f:
     data = [[int(cell.strip()) for cell in row.rstrip(' ').split(' ')] for row in f]
 
-def horizontal_sums(data):
+tdata = [list(i) for i in zip(*data)]
+edata = enumerate(data)
+
+def hv_product(data):
     largest_sum = []
     for row in data:
         sum_list = []
-        for i in range(16):
+        for i in range(15):
             sum_list.append(row[i]*row[i+1]*row[i+2]*row[i+3])
         largest_sum.append(max(sum_list))
     return max(largest_sum)
 
-def vertical_sums(data):
-    largest_sum = []
-    for i in range(19):
-        sum_list = []
-        for j in range(17):
-            sum_list.append(data[i][j]*data[i][j+1]*data[i][j+2]*data[i][j+3])
-        largest_sum.append(max(sum_list))
-    return max(largest_sum)
+for row, count in zip(edata, range(1,6)):
+    sum_list = []
+    for i in range(15):
+        sum_list.append(row[1][i] * edata[count][i+1] * edata[count+1][i+2])
 
-def up_horizontal_sums(data):
+def up_horizontal_sums():
     largest_sum = []
     for i in range(17):
         sum_list = []
@@ -34,7 +33,7 @@ def up_horizontal_sums(data):
         largest_sum.append(max(sum_list))
     return max(largest_sum)
 
-def down_horizontal_sums(data):
+def down_horizontal_sums():
     largest_sum = []
     for i in range(17):
         sum_list = []
@@ -42,3 +41,4 @@ def down_horizontal_sums(data):
             sum_list.append(data[19-i][19-j]*data[19-i-1][19-j-1]*data[19-i-2][19-j-2]*data[19-i-3][19-j-3])
         largest_sum.append(max(sum_list))
     return max(largest_sum)
+
